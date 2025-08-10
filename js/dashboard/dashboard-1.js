c06e5de8ccb56b2d13318415e849afc862282137
FORTELalejandro.montalvan <alejandro.montalvan@fortelcorp.com>
2025-08-10 10:19:00 -0500

    now year scrit commit

--- js/dashboard/dashboard-1.js
+++ js/dashboard/dashboard-1.js
@@ -1,6 +1,10 @@
 (function($) {
-    /* "use strict" */
-
+  "use strict"
+  jQuery(window).on('load',function(){
+    setTimeout(function(){
+      assignedDoctor();
+    }, 1000); 
+  });
 
  var dzChartlist = function(){
 	
@@ -203,3 +207,42 @@
 	});     
 
 })(jQuery);
+
+function assignedDoctor()
+{
+
+  /*  testimonial one function by = owl.carousel.js */
+  jQuery('.assigned-doctor').owlCarousel({
+    loop:false,
+    margin:30,
+    nav:true,
+    autoplaySpeed: 3000,
+    navSpeed: 3000,
+    paginationSpeed: 3000,
+    slideSpeed: 3000,
+    smartSpeed: 3000,
+    autoplay: false,
+    dots: false,
+    navText: ['<i class="fa fa-caret-left"></i>', '<i class="fa fa-caret-right"></i>'],
+    responsive:{
+      0:{
+        items:1
+      },
+      576:{
+        items:2
+      },	
+      767:{
+        items:3
+      },			
+      991:{
+        items:2
+      },
+      1200:{
+        items:3
+      },
+      1600:{
+        items:5
+      }
+    }
+  })
+}
